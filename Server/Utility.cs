89ec594a0395789724264dd7a83a04ba448fe061
liu <liupanfengfreedom@gmail.com>
2019-08-12 10:10:49 +0800

    addutility

---
+++ Server/Utility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Server
+{
+    class Utility
+    {
+        static string path = @"C:\Program Files\Epic Games\UE_4.22\Engine\Build\BatchFiles\RunUAT.bat";
+        static string Arguments = "BuildCookRun -project=D:\\ueprojecttest/MyProject/MyProject.uproject  -noP4 -platform=Android -clientconfig=Development -serverconfig=Development -cook -allmaps -stage -pak -archive";
+
+        public static void CommandRun(string exe, string arguments)
+        {
+            try
+            {
+                ProcessStartInfo info = new ProcessStartInfo
+                {
+                    CreateNoWindow = true,
+                    FileName = exe,
+                    Arguments = arguments,
+                    UseShellExecute = true,//true mean can launch .bat file
+                };
+                Process p = Process.Start(info);
+                //p.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                // Log.Error(e);
+            }
+        }
+    }
+}
